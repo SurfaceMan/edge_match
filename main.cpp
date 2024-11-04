f8fcd0b700e40d99286a832ef69bc1a663474bf4
y.qiu <y.qiu@local>
2024-11-04 17:57:02 +0800

    misc

--- main.cpp
+++ main.cpp
@@ -1,23 +1,32 @@
 #include "edgesubpix.h"
 
-constexpr int MIN_AREA = 256;
+constexpr int MIN_AREA  = 256;
+constexpr int CANDIDATE = 5;
+
+struct Pose {
+    float x;
+    float y;
+    float angle;
+    float score;
+};
 
 struct Candidate {
     float       score;
     float       angle;
     cv::Point2f pos;
+
+    bool operator<(const Candidate &rhs) const {
+        return this->score > rhs.score;
+    }
 };
 
 struct Template {
-    cv::Size                 size;
     std::vector<cv::Point2f> edges;
     std::vector<float>       angles;
 };
 
 struct Layer {
-    double   startAngle = 0;
-    double   stopAngle  = 0;
-    double   angleStep  = 0;
+    float    angleStep;
     Template templates;
 };
 
@@ -41,16 +50,12 @@ struct Model {
     uchar     minMag;
     Metric    metric;
     Reduce    reduce;
+    float     radius;
+    float     startAngle;
+    float     stopAngle;
 
     cv::Mat            source;
     std::vector<Layer> layels;
-};
-
-struct Pose {
-    float x;
-    float y;
-    float angle;
-    float score;
 };
 
 // inline double sizeAngleStep(const cv::Size &size) {
@@ -94,31 +99,39 @@ void nextMaxLoc(cv::Mat         &score,
     cv::minMaxLoc(score, nullptr, &maxScore, nullptr, &maxPos);
 }
 
-Model trainModel(const cv::Mat &src,
-                 int            numLevels,
-                 float          angleStart,
-                 float          angleExtent,
-                 float          angleStep,
-                 Reduce         reduce,
-                 Metric         metric,
-                 EdgeParam      edgeParam,
-                 uchar          minMag) {
-    if (src.empty() || src.channels() != 1) {
-        return {};
+Template buildTemplate(const cv::Mat     &src,
+                       const EdgeParam   &edgeParam,
+                       const cv::Point2f &offset,
+                       cv::InputArray     mask = cv::noArray()) {
+    std::vector<std::vector<cv::Point2f>> edges;
+    std::vector<std::vector<cv::Vec2f>>   dirs;
+    EdgePoint(src, edges, dirs, edgeParam.sigma, edgeParam.low, edgeParam.high, mask);
+
+    std::vector<cv::Point2f> points;
+    std::vector<float>       angles;
+    for (std::size_t i = 0; i < edges.size(); i++) {
+        const auto &edge = edges[ i ];
+        const auto &dir  = dirs[ i ];
+        if (edgeParam.minLength > edge.size()) {
+            continue;
+        }
+
+        std::vector<float> subAngles(dir.size());
+        std::transform(dir.begin(), dir.end(), subAngles.begin(), [](const cv::Vec2f &vec) {
+            return atan2f(vec[ 1 ], vec[ 0 ]);
+        });
+
+        points.insert(points.end(), edge.begin(), edge.end());
+        angles.insert(angles.begin(), subAngles.begin(), subAngles.end());
     }
 
-    if (numLevels <= 0) {
-        // level must greater than 0
-        numLevels = computeLayers(src.size().width, src.size().height, MIN_AREA);
-    }
+    std::for_each(points.begin(), points.end(), [ & ](cv::Point2f &point) { point -= offset; });
+    return {points, angles};
+}
 
-    const auto scale   = 1 << (numLevels - 1);
-    const auto topArea = src.size().area() / (scale * scale);
-    if (MIN_AREA > topArea) {
-        // top area must greater than MIN_AREA
-        return {};
-    }
+cv::Mat matchTemplate(const cv::Mat &angle, const Template &temp, float rotation) {}
 
+std::vector<cv::Mat> buildPyramid(const cv::Mat &src, int numLevels) {
     auto        srcWidth      = static_cast<std::size_t>(src.cols);
     auto        srcHeight     = static_cast<std::size_t>(src.rows);
     std::size_t step          = 1 << (numLevels - 1);
@@ -149,6 +162,147 @@ Model trainModel(const cv::Mat &src,
 
         pyramids.emplace_back(std::move(tmp));
     }
+
+    return pyramids;
+}
+
+#pragma omp declare reduction(combine : std::vector<Candidate> : omp_out                           \
+                                  .insert(omp_out.end(), omp_in.begin(), omp_in.end()))
+
+std::vector<Candidate> matchTopLayel(const cv::Mat &dstTop,
+                                     float          startAngle,
+                                     float          spanAngle,
+                                     float          maxOverlap,
+                                     float          minScore,
+                                     int            maxCount,
+                                     const Model   &model,
+                                     int            numLevels) {
+    std::vector<Candidate> candidates;
+
+    const auto &templateTop       = model.layels[ numLevels - 1 ];
+    const auto  topScoreThreshold = minScore * pow(0.9, numLevels - 1);
+    const auto  angleStep         = templateTop.angleStep;
+    const auto  count             = static_cast<int>(spanAngle / angleStep) + 1;
+
+    cv::Mat dx;
+    cv::Mat dy;
+    cv::spatialGradient(dstTop, dx, dy);
+
+    dx.convertTo(dx, CV_32FC1);
+    dy.convertTo(dy, CV_32FC1);
+
+    cv::Mat mag;
+    cv::Mat angle;
+    cv::cartToPolar(dx, dy, mag, angle);
+
+#pragma omp parallel for reduction(combine : candidates)
+    for (int i = 0; i < count; i++) {
+        const auto rotation = startAngle + angleStep * i;
+
+        auto result = matchTemplate(angle, templateTop.templates, rotation);
+
+        double    maxScore;
+        cv::Point maxPos;
+        cv::minMaxLoc(result, nullptr, &maxScore, nullptr, &maxPos);
+        if (maxScore < topScoreThreshold) {
+            continue;
+        }
+
+        candidates.emplace_back(maxScore, angle, cv::Point2f(maxPos));
+        for (int j = 0; j < maxCount + CANDIDATE - 1; j++) {
+            nextMaxLoc(result, maxPos, templateTop.size(), maxOverlap, maxScore, maxPos);
+            if (maxScore < topScoreThreshold) {
+                break;
+            }
+
+            candidates.emplace_back(maxScore, angle, cv::Point2f(maxPos));
+        }
+    }
+
+    std::sort(candidates.begin(), candidates.end());
+
+    return candidates;
+}
+
+std::vector<Candidate> matchDownLayel(const std::vector<cv::Mat>   &pyramids,
+                                      const std::vector<Candidate> &candidates,
+                                      double                        minScore,
+                                      int                           subpixel,
+                                      const Model                  &model,
+                                      int                           numLevels) {
+    std::vector<Candidate> levelMatched;
+    auto                   count = static_cast<int>(candidates.size());
+
+#pragma omp parallel for reduction(combine : levelMatched)
+    for (int index = 0; index < count; index++) {
+        auto pose    = candidates[ index ];
+        bool matched = true;
+
+        for (int currentLevel = numLevels - 1; currentLevel >= 0; currentLevel--) {
+            const auto &currentLayel   = model.layels[ currentLevel ];
+            const auto  scoreThreshold = minScore * pow(0.9, currentLevel);
+            const auto  angleStep      = currentLayel.angleStep;
+
+            for (int i = -1; i <= 1; i++) {
+                auto rotation = pose.angle + i * angleStep;
+
+                auto result = matchTemplate(angle, currentLayel.templates, rotation);
+            }
+        }
+    }
+}
+
+Model trainModel(const cv::Mat &src,
+                 int            numLevels,
+                 float          angleStart,
+                 float          angleExtent,
+                 float          angleStep,
+                 Reduce         reduce,
+                 Metric         metric,
+                 EdgeParam      edgeParam,
+                 uchar          minMag) {
+    if (src.empty() || src.channels() != 1) {
+        return {};
+    }
+
+    if (numLevels <= 0) {
+        // level must greater than 0
+        numLevels = computeLayers(src.size().width, src.size().height, MIN_AREA);
+    }
+
+    const auto scale   = 1 << (numLevels - 1);
+    const auto topArea = src.size().area() / (scale * scale);
+    if (MIN_AREA > topArea) {
+        // top area must greater than MIN_AREA
+        return {};
+    }
+
+    auto pyramids = buildPyramid(src, numLevels);
+
+    // build bottom template
+    cv::Point2f center(0, 0);
+    float       radius       = 0;
+    auto        baseTemplate = buildTemplate(pyramids.front(), edgeParam, center);
+    cv::minEnclosingCircle(baseTemplate.edges, center, radius);
+    std::for_each(baseTemplate.edges.begin(), baseTemplate.edges.end(), [ & ](cv::Point2f &point) {
+        point -= center;
+    });
+
+    if (angleStep < 0) {
+        angleStep = atan(1 / radius);
+    }
+
+    auto  stopAngle = angleStart + angleExtent;
+    Model model{edgeParam, minMag, metric, reduce, radius, angleStart, stopAngle, src};
+    model.layels.emplace_back(angleStep, baseTemplate);
+    for (std::size_t i = 1; i < pyramids.size(); i++) {
+        center    /= 2.f;
+        angleStep *= 2.f;
+
+        model.layels.emplace_back(angleStep, buildTemplate(pyramids[ i ], edgeParam, center));
+    }
+
+    return model;
 }
 
 std::vector<Pose> matchModel(const cv::Mat &dst,
@@ -156,86 +310,39 @@ std::vector<Pose> matchModel(const cv::Mat &dst,
                              float          angleStart,
                              float          angleExtent,
                              float          angleStep,
+                             float          minScore,
                              int            numMatches,
                              float          maxOverlap,
                              bool           subpixel,
                              int            numLevels,
-                             float          greediness) {}
+                             float          greediness) {
+    if (dst.empty() || model.layels.empty()) {
+        return {};
+    }
+
+    const auto templateLevel = static_cast<int>(model.layels.size());
+    if (numLevels < 0 || numLevels > templateLevel) {
+        numLevels = templateLevel;
+    }
+
+    if (angleStart > model.startAngle) {
+        angleStart = model.startAngle;
+    }
+
+    if (angleStart + angleExtent > model.stopAngle) {
+        angleExtent = model.stopAngle - angleStart;
+    }
+
+    auto pyramids = buildPyramid(dst, numLevels);
+
+    // compute top
+    const std::vector<Candidate> candidates = matchTopLayel();
+
+    // match candidate each layel
+    std::vector<Candidate> matched = matchDownLayel();
+}
 
 int main(int argc, const char *argv[]) {
-    if (argc < 3) {
-        std::cout << "Too few arguments" << std::endl;
-        return -1;
-    }
-
-    auto src = cv::imread(argv[ 1 ], cv::IMREAD_GRAYSCALE);
-
-    auto        srcWidth      = static_cast<std::size_t>(src.cols);
-    auto        srcHeight     = static_cast<std::size_t>(src.rows);
-    std::size_t level         = 4;
-    std::size_t step          = 1 << (level - 1);
-    auto        alignedWidth  = cv::alignSize(srcWidth, (int)step);
-    auto        alignedHeight = cv::alignSize(srcHeight, (int)step);
-
-    std::size_t paddWidth  = alignedWidth - srcWidth;
-    std::size_t paddHeight = alignedHeight - srcHeight;
-
-    // template
-    std::vector<cv::Mat> pyramids;
-    cv::Mat              templateImg = src;
-    if (0 != paddHeight || 0 != paddWidth) {
-        cv::copyMakeBorder(src,
-                           templateImg,
-                           0,
-                           (int)paddWidth,
-                           0,
-                           (int)paddHeight,
-                           cv::BORDER_REFLECT);
-    }
-
-    pyramids.emplace_back(std::move(templateImg));
-    for (std::size_t i = 0; i < level - 1; i++) {
-        const auto &last = pyramids[ i ];
-        cv::Mat     tmp;
-        cv::resize(last, tmp, last.size() / 2, 0, 0, cv::INTER_AREA);
-
-        pyramids.emplace_back(std::move(tmp));
-    }
-
-    std::vector<std::vector<cv::Point2f>> edge4;
-    std::vector<std::vector<cv::Vec2f>>   dir4;
-    EdgePoint(templateImg4, edge4, dir4, 1., 10, 29);
-
-    cv::Point2f center((templateImg4.cols - 1) / 2.f, (templateImg4.rows - 1) / 2.f);
-    auto        angleStep = sizeAngleStep(templateImg4.size());
-
-    std::vector<std::vector<cv::Point2f>> points4;
-    std::vector<std::vector<float>>       angles4;
-
-    const int minFeatureLength = 5;
-    for (int i = 0; i < dir4.size(); i++) {
-        const auto &feature = dir4[ i ];
-        if (feature.size() < minFeatureLength) {
-            continue;
-        }
-
-        const auto              &edge = edge4[ i ];
-        std::vector<cv::Point2f> points;
-        points.reserve(edge.size());
-        for (const auto &point : edge) {
-            points.emplace_back(point - center);
-        }
-        points4.emplace_back(std::move(points));
-
-        std::vector<float> angles;
-        for (const auto &dir : feature) {
-            auto angle = atan2f(dir[ 1 ], dir[ 0 ]);
-            angles.push_back(angle);
-        }
-
-        angles4.emplace_back(std::move(angles));
-    }
-
     // scene
     auto dst = cv::imread(argv[ 2 ], cv::IMREAD_GRAYSCALE);
 
